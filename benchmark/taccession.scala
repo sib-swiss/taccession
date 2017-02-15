32e32682dff40ad3152ae1894c562426c82ead3e
Daniel Teixeira <ddtxra@gmail.com>
2017-02-15 18:24:19 +0100

    Update taccession.scala

--- benchmark/taccession.scala
+++ benchmark/taccession.scala
@@ -1,6 +1,10 @@
+//Starts the time
 val start = System.currentTimeMillis();
+
+//Defines the directory where the publications are stored
 val PUBLI_DIR = "/scratch/cluster/monthly/dteixeir/publis/";
 
+//Creates a map with several regular expressions
 val patterns = Map(
     "uniprot" -> "[OPQ][0-9][A-Z0-9]{3}[0-9]|[A-NR-Z][0-9]([A-Z][A-Z0-9]{2}[0-9]){1,2}".r,
     "string" -> "^([A-N,R-Z][0-9][A-Z][A-Z, 0-9][A-Z, 0-9][0-9])|([O,P,Q][0-9][A-Z, 0-9][A-Z, 0-9][A-Z, 0-9][0-9])|([0-9][A-Za-z0-9]{3})$".r,
@@ -14,8 +18,11 @@ val patterns = Map(
     //Cellosorus / CALOHA ? SwissRegulon
 )
 
+//Creates a domain class for manipulation and exporting the result.
+//A word found in a publication for a certain entity (defined in patterns) at a line in a offset and with a length.
 case class TokenMatch(word: String, publication: String, entity: String, line: Integer, offset: Integer, length: Integer)
 
+//Function that opens a file, search for the patterns and returns a list of TokenMatch with the results
 def searchTokens(fileName: String): List[TokenMatch] = {
 
     val file = scala.io.Source.fromFile(PUBLI_DIR + fileName);
@@ -42,13 +49,20 @@ def searchTokens(fileName: String): List[TokenMatch] = {
    return result
 }
 
+//Change this value to have different results
+val minPartitions = 200;
 
-val allFiles = sc.textFile(PUBLI_DIR + "file_names.txt", 200)
+//Reads a file containing alls files to be parsed
+val allFiles = sc.textFile(PUBLI_DIR + "file_names.txt", minPartitions)
+
+//Reads all files
 val df = allFiles.flatMap(searchTokens(_)).toDF()
 
 val result = df.filter($"entity" !== "string").filter($"entity" !== "uniprot").groupBy($"word", $"entity").agg(count("*") as "numOccurances").orderBy($"numOccurances" desc)
 
 val start2 = System.currentTimeMillis();
+
+//Gets the first 10 results
 result.take(10)
 
 println("Finished in " + (System.currentTimeMillis() - start) / (60 * 1000.0)  + " min")
