69475d6a11fc4e340d8d6b3426709af3bf0408f0
Daniel Teixeira <ddtxra@gmail.com>
2017-04-05 18:36:35 +0200

    Update taccession-save-json.scala

--- script/taccession-save-json.scala
+++ script/taccession-save-json.scala
@@ -18,7 +18,9 @@ val patterns = TaccessionConfig.getPatterns(config);
 //Reads all files (this is distributed among all workers)
 val df = filesRDD.flatMap(f => Taccession.searchTokens(patterns, f)).toDF()
 
+val outputFolder = TaccessionConfig.getOutputFolder(config);
+
 //Save results in json format
-df.write.json("result-json")
+df.write.json(outputFolder + "/json-output")
 
 println("Finished in " + (System.currentTimeMillis() - start) / (60 * 1000.0)  + " min")
