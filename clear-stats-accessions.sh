e6567e781f3b2810f375730b7f035ce805f12fa3
Daniel <ddtxra@gmail.com>
2017-04-26 18:48:04 +0200

    adding new variants

--- clear-stats-accessions.sh
+++ clear-stats-accessions.sh
@@ -1,4 +1,2 @@
 rm -rf stats/stats-accession/stats-csv-
 git rm -r stats/stats-accession/stats-csv-
-rm -rf stats/stats-variants/stats-csv-
-git rm -r stats/stats-variants/stats-csv-
---
+++ clear-stats-variants.sh
@@ -0,0 +1,2 @@
+rm -rf stats/stats-variants/stats-csv-
+git rm -r stats/stats-variants/stats-csv-
