b55d771d186a1b1e2874269b712e2fec9d24ed7f
Daniel Teixeira <ddtxra@gmail.com>
2017-05-22 18:34:37 +0200

    changing script for saving data

--- script/taccession-save-data.scala
+++ script/taccession-save-data.scala
@@ -3,24 +3,19 @@ import swiss.sib.taccession._
 //Starts the time
 val start = System.currentTimeMillis();
 
-//Init configs
-val config = TaccessionConfig.init(System.getProperty("config.file"))
 
-//File containing all path of the files to parse (a trick for spark going faster)
-val filePaths = TaccessionConfig.getFilePaths(config);
+//Init configs
+val config = TaccessionUtils.readConfigFile(System.getProperty("config.file"))
+
+val filePaths = TaccessionUtils.getFilePaths(config)
 
 //Reads paths 
-val filesRDD = sc.textFile(filePaths, TaccessionConfig.getMinPartitions(config))
-
-//Patterns
-val patterns = TaccessionConfig.getPatterns(config);
+val filesRDD = sc.textFile(filePaths, config.sparkPartitions)
 
 //Reads all files (this is distributed among all workers)
-val df = filesRDD.flatMap(f => Taccession.searchTokens(patterns, f)).toDF()
-
-val outputFolder = TaccessionConfig.getOutputFolder(config);
+val df = filesRDD.flatMap(f => Taccession.searchTokens(config.patterns, f)).toDF()
 
 //Save results in json format
-df.select("matchedPattern", "matchedPatternLength", "lineNumber", "columnNumber", "publicationName", "patternName").write.json(outputFolder + "/json-output")
+df.select("matchedPattern", "matchedPatternLength", "lineNumber", "columnNumber", "publicationName", "patternName").write.json(config.dataOutputFolder.getPath + "/json-output")
 
 println("Finished in " + (System.currentTimeMillis() - start) / (60 * 1000.0)  + " min")
