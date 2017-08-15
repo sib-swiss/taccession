dd977ffb608488bc861862ade9d81b7457c1e6ec
Daniel Teixeira <ddtxra@gmail.com>
2017-08-15 14:33:51 +0200

    Update spark-master-lsf.sh

--- lsf/spark-master-lsf.sh
+++ lsf/spark-master-lsf.sh
@@ -14,7 +14,7 @@ spark_release=spark-2.2.0-bin-hadoop2.7
 module add Development/java_jdk/1.8.0_112
 mkdir -p /scratch/local/daily/$USER/$spark_release
 rm -r /scratch/local/daily/$USER/$spark_release/*
-cp -r /scratch/cluster/monthly/$USER/$spark_release /scratch/local/daily/$USER/
+cp -r /scratch/cluster/monthly/spark/$spark_release /scratch/local/daily/$USER/
 cd /scratch/local/daily/$USER/$spark_release
 
 rm -r /scratch/local/daily/$USER/spark-notebook
